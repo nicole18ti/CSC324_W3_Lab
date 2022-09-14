6106a529364d6f752fea083aba1a3ef29cfe5415
nicolechan1118@gmail.com <nicolechan@localhost>
2022-09-14 10:40:26 -0400

    Add Freshman and person class and override method

---
+++ edu/farmingdale/csc325_f22_week_lab/Freshman.java
@@ -0,0 +1,22 @@
+
+package edu.farmingdale.csc325_f22_week_lab;
+
+// Freshman class is a child of Student
+public class Freshman extends Student implements Person{
+
+    // The override will override the method 
+    @Override
+    public void setAddress(String s) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void getAddress(String s) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+
+    
+    
+}
