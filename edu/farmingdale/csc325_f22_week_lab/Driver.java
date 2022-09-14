f21c3a44cbeccb673f73af54f2523a05f6ff8b39
nicolechan1118@gmail.com <nicolechan@localhost>
2022-09-14 10:31:45 -0400

    add both together and add setFname

--- edu/farmingdale/csc325_f22_week_lab/Driver.java
+++ edu/farmingdale/csc325_f22_week_lab/Driver.java
@@ -5,6 +5,9 @@ package edu.farmingdale.csc325_f22_week_lab;
 public class Driver {
     
     public static void main(String[] args) {
+        Student std = new Student();
+        std.setfName = "Ben";
+        
         System.out.println("hi");
         
     }
